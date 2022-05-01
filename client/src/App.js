d4df4504c25610e748e7d58d8c808bae9ba34869
Tampoka <goal.thanks@gmail.com>
2022-05-01 12:20:02 +0900

    Add SelectedFoods component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss'
+import {Component} from 'react';
+import SelectedFoods from './components/SelectedFoods/SelectedFoods';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        selectedFoods: [],
+    }
+
+    render() {
+        return (
+        <div className={s.app}>
+            <div className={s.container}>
+                <SelectedFoods
+                foods={this.state.selectedFoods}
+                onFoodClick={
+                    (index)=>(
+                    this.setState({
+                        selectedFoods:[
+                            ...this.state.selectedFoods.slice(0,index),
+                            ...this.state.selectedFoods.slice(index+1,this.state.selectedFoods.length),
+                        ],
+                    })
+                )}/>
+            </div>
+        </div>
+        )
+    }
 }
 
 export default App;
---
+++ client/src/components/SelectedFoods/SelectedFoods.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import s from './SelectedFoods.module.scss'
+
+const SelectedFoods = (props) => {
+    return (
+        <table className={s.table}>
+            <thead>
+            <tr>
+                <th colSpan='5'>
+                    <h3>Selected foods</h3>
+                </th>
+            </tr>
+            <tr>
+                <th>Description</th>
+                <th>Kcal</th>
+                <th>Protein (g)</th>
+                <th>Fat (g)</th>
+                <th>Carbs (g)</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                props.foods.map((food, index) => (
+                    <tr key={index}
+                        onClick={() => props.onFoodClick(index)}
+                    >
+                        <td>{food.description}</td>
+                        <td>{food.kcal}</td>
+                        <td>{food.protein_g}</td>
+                        <td>{food.fat_g}</td>
+                        <td>{food.carbohydrate_g}</td>
+                    </tr>
+                ))
+            }
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Total</th>
+                <th>{sum(props.foods, 'kcal')}</th>
+                <th> {sum(props.foods, 'protein_g')}</th>
+                <th>{sum(props.foods, 'fat_g')}</th>
+                <th> {sum(props.foods, 'carbohydrate_g')}</th>
+
+            </tr>
+            </tfoot>
+        </table>
+    );
+};
+
+// Good example of a helper function specific to this module
+// that is inaccessible from outside this module.
+function sum(foods, prop) {
+    return foods.reduce((memo, food) => (
+        parseInt(food[prop], 10) + memo
+    ), 0.0).toFixed(2);
+}
+
+export default SelectedFoods;
